4f45783ef3944e351e047210353cd9f87ebf83fd
alenapech <alenaryzhova@inbox.ru>
2022-06-09 23:26:58 +0400

    Practice 1

---
+++ Task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+/*if (a >= b) {
+    if (a > c) {
+        Console.WriteLine(a);
+    } else {
+        Console.WriteLine(c);
+    }
+} else {
+    if (b > c) {
+        Console.WriteLine(b);
+    } else {
+        Console.WriteLine(c);
+    }
+}*/
+
+int res = a;
+if (b > res) {
+    res = b;
+}
+if (c > res) {
+    res = c;
+}
+Console.WriteLine(res);
---
+++ Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (n <= i) {
+    Console.WriteLine("Четных чисел нет");
+}
+
+while (i <= n) {
+    if (i%2 == 0) {
+        Console.WriteLine(i);
+    }
+    i++;
+}
